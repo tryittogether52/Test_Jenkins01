8473ba70506586939b73b2b6f0608521e41495fe
tryittogether52 <tryittogether52@gmail.com>
2015-10-28 04:02:38 +0000

    add foo function

--- src/MyTestSuite1.h
+++ src/MyTestSuite1.h
@@ -1,11 +1,22 @@
 #include <cxxtest/TestSuite.h>
-
+  int foo(int param)
+    {
+         if (param)
+         {
+             return 1;
+         }
+         else
+        {
+            return 0;
+        }
+    }
+   
  class MyTestSuite : public CxxTest::TestSuite
  {
  public:
      void testAddition( void )
      {
-         TS_ASSERT( 1 + 1 > 1 );
-         TS_ASSERT_EQUALS( 1 + 1, 2 );
+//         TS_ASSERT( 1 + 1 > 1 );
+         TS_ASSERT_EQUALS( foo(0),0  );
      }
  };
